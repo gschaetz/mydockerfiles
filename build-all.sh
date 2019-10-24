d8ec5c3083280046b8be197e29e464af1cf531d1
Gary Schaetz <gary@gary.schaetzkc.com>
2019-10-24 09:53:44 -0500

    test

--- build-all.sh
+++ build-all.sh
@@ -74,7 +74,7 @@ main(){
 	do 
 		IFS=$'\n'
 		temp=( $(curl https://registry.hub.docker.com/v2/repositories/gschaetz/$i  2>/dev/null|jq '.last_updated+","+.name' | sed 's/"//g') ) > /dev/null 2>&1
-		curl -vvv https://registry.hub.docker.com/v2/repositories/gschaetz/$i  2>/dev/null|jq '.last_updated+","+.name' | sed 's/"//g') 		
+		curl -vvv https://registry.hub.docker.com/v2/repositories/gschaetz/$i  2>/dev/null|jq '.last_updated+","+.name' | sed 's/"//g' 		
 		echo $temp
 		unset IFS
 		if [[ $temp == '","' ]]; then
